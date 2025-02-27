030a6b3681ea4a9305d6aa1bbf9b7f4fd479c151
Rajan66 <rajanmaharjan042@gmail.com>
2025-02-27 23:14:17 +0545

    feat: allow vercel site

--- core/core/settings.py
+++ core/core/settings.py
@@ -32,6 +32,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
     "http://0.0.0.0:3000",
+    "https://shoppy-ashen.vercel.app"
 ]
 
 CORS_ALLOW_CREDENTIALS = True
