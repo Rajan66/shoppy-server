25436e6eadf7eb641c8e6941e14abe207f8fd122
Rajan66 <rajanmaharjan042@gmail.com>
2025-02-27 21:20:32 +0545

    chore: add gender field and alter cart

--- core/products/models.py
+++ core/products/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Product(models.Model):
     title = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
@@ -15,6 +16,7 @@ class Product(models.Model):
 class Category(models.Model):
     title = models.CharField(max_length=255)
     imageUrl = models.ImageField(upload_to='category_images/')
+    gender = models.CharField(max_length=20, blank=True)
 
     def __str__(self):
         return self.title
